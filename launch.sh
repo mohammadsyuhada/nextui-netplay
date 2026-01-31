be50527b8007623c63ffff9f723fa9bd0ae1d3ac
mohammadsyuhada <mohammadsyuhada@gmail.com>
2026-01-31 11:13:17 +0800

    fix: missing build binary for tg5050

--- launch.sh
+++ launch.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 DIR="$(dirname "$0")"
 cd "$DIR"
+
+# Use system PLATFORM variable, fallback to tg5040 if not set
 [ -z "$PLATFORM" ] && PLATFORM="tg5040"
-export LD_LIBRARY_PATH="$DIR:$DIR/bin:$LD_LIBRARY_PATH"
+
+export LD_LIBRARY_PATH="$DIR:$DIR/bin:$DIR/bin/$PLATFORM:$LD_LIBRARY_PATH"
 export HOME="/mnt/SDCARD/.userdata/$PLATFORM"
-"$DIR/bin/netplay.elf" &> "$DIR/log.txt"
+
+# Run the platform-specific binary
+"$DIR/bin/$PLATFORM/netplay.elf" &> "$DIR/log.txt"
